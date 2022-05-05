9331f5685614922afecf0a1268e084884c61b6e6
ViniciusGavaPereira <74336299+ViniciusGavaPereira@users.noreply.github.com>
2022-05-05 15:38:22 -0300

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/entity/Cliente.java
@@ -0,0 +1,42 @@
+package com.example.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "cliente")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idCliente")
+    private Integer idCliente;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "cpf")
+    private String cpf;
+
+    @Column(name = "telefone")
+    private String telefone;
+
+    @Column(name = "senha")
+    private String senha;
+
+}
